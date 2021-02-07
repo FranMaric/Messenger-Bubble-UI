496e2fd96333e0df1e8a42a894f0b7675a0ffae8
Fran Marić (komp-VK) <fm.develoloper@gmail.com>
2021-02-07 18:11:23 +0100

    Cleaned up main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:math';
+import 'constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,9 +16,9 @@ class MyApp extends StatelessWidget {
     ]);
 
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.lime[400],
+      statusBarColor: background,
       statusBarIconBrightness: Brightness.dark,
-      systemNavigationBarColor: Colors.lime[400],
+      systemNavigationBarColor: background,
     ));
 
     return MaterialApp(
@@ -29,8 +29,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-const double radius = 30;
-
 class Messenger extends StatefulWidget {
   @override
   _MessengerState createState() => _MessengerState();
@@ -38,93 +36,91 @@ class Messenger extends StatefulWidget {
 
 class _MessengerState extends State<Messenger> {
   bool fingerDown = false;
-  double messageBubbleX = 0;
-  double messageBubbleY = 0;
+  double messageBubbleX = 0, messageBubbleY = 0;
 
-  double exitBubbleX;
-  double exitBubbleY;
+  double exitBubbleX, exitBubbleY;
+
+  double screenWidth, screenHeight;
 
   @override
   Widget build(BuildContext context) {
-    if (exitBubbleX == null || exitBubbleY == null) {
-      exitBubbleX = MediaQuery.of(context).size.width / 2 - radius;
-      exitBubbleY = MediaQuery.of(context).size.height - 120;
+    if (screenWidth == null || screenHeight == null) {
+      screenWidth = MediaQuery.of(context).size.width;
+      screenHeight = MediaQuery.of(context).size.height;
     }
+
+    void onStart(DragStartDetails details) {
+      fingerDown = true;
+      exitBubbleY = screenHeight - 120;
+    }
+
+    void onUpdate(DragUpdateDetails details) {
+      messageBubbleX += details.delta.dx;
+      messageBubbleY += details.delta.dy;
+
+      setState(() {});
+    }
+
+    void onEnd(DragEndDetails details) {
+      fingerDown = false;
+
+      if (messageBubbleX + radius <= screenWidth / 2) {
+        messageBubbleX = 0;
+      } else {
+        messageBubbleX = screenWidth - radius * 2;
+      }
+
+      exitBubbleY = screenHeight;
+
+      setState(() {});
+    }
+
     return Scaffold(
       body: SafeArea(
         child: Stack(
           children: [
             Container(
-              color: Colors.lime[400],
+              color: background,
             ),
             AnimatedPositioned(
               duration: Duration(milliseconds: fingerDown ? 0 : 200),
               left: messageBubbleX,
               top: messageBubbleY,
               child: GestureDetector(
-                onPanCancel: () {
-                  fingerDown = false;
-
-                  if (messageBubbleX + radius <=
-                      MediaQuery.of(context).size.width / 2) {
-                    messageBubbleX = 0;
-                  } else {
-                    messageBubbleX =
-                        MediaQuery.of(context).size.width - radius * 2;
-                  }
-                  setState(() {});
-                },
-                onPanStart: (details) => fingerDown = true,
-                onTapDown: (details) => fingerDown = true,
-                onPanEnd: (details) {
-                  fingerDown = false;
-
-                  if (messageBubbleX + radius <=
-                      MediaQuery.of(context).size.width / 2) {
-                    messageBubbleX = 0;
-                  } else {
-                    messageBubbleX =
-                        MediaQuery.of(context).size.width - radius * 2;
-                  }
-                  setState(() {});
-                },
-                onPanUpdate: (details) => setState(() {
-                  messageBubbleX += details.delta.dx;
-                  messageBubbleY += details.delta.dy;
-                }),
+                onPanStart: onStart,
+                onPanUpdate: onUpdate,
+                onPanEnd: onEnd,
                 child: Container(
                   width: radius * 2,
                   height: radius * 2,
                   decoration: BoxDecoration(
-                    color: Colors.blue[300],
+                    color: bubbleColor,
                     shape: BoxShape.circle,
                   ),
                 ),
               ),
             ),
-            Visibility(
-              visible: fingerDown,
-              child: Positioned(
-                left: exitBubbleX,
-                top: exitBubbleY,
-                child: Container(
-                  width: radius * 2,
-                  height: radius * 2,
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade400,
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 1,
-                    ),
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 100),
+              left: exitBubbleX,
+              top: exitBubbleY,
+              child: Container(
+                width: radius * 2,
+                height: radius * 2,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade400,
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
                   ),
-                  alignment: Alignment.center,
-                  child: Text(
-                    'X',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w400,
-                    ),
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  'X',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
               ),
